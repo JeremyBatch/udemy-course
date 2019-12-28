40be60e7287c7e6e59d6384b2114e40c17ed71ad
Jeremy Batchelor <jdbatchelor85@outlook.com>
2019-12-28 10:22:27 -0600

    first project completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import UserInput from "./UserInput/UserInput";
+import UserOutput from "./UserOutput/UserOutput";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    username: "supermax"
+  };
+
+  usernameChangedHandler = (event) => {
+    this.setState({username: event.target.value})
+  }
+  render() {
+    return (
+      <div className="App">
+        <UserInput 
+          changed={this.usernameChangedHandler}
+          currentName={this.state.username}/>
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        
+      </div>
+    );
+  }
 }
 
 export default App;
